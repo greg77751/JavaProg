074d1fb6a69670681b327ab19ab1418efbb404bf
Greg <greg77751@gmail.com>
2017-03-05 12:59:21 +0100

    upd

--- Z1/src/Program.java
+++ Z1/src/Program.java
@@ -4,5 +4,9 @@
 public class Program {
     public static void main(String [] args)    {
         System.out.print("x");
+        double[] tab = new double[3];
+        tab[0] = 6.6;
+        System.out.print(tab[0]);
+
     }
 }
